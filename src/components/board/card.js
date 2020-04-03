c6917ab638f2d106215e43d3d64b70ddd5812465
leny <info@flatland.be>
2020-04-03 14:43:01 +0200

    Basic styles for BoardCard

--- src/components/board/card.js
+++ src/components/board/card.js
@@ -9,13 +9,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import {px, absolute, relative, size} from "koutla-swiss";
+
 import cardBack from "../../assets/game/card-back-board.png";
 import cardBackAlt from "../../assets/game/card-back-board-alt.png";
 
+const styles = {
+    card: {
+        ...size(px(120)),
+        ...relative(),
+    },
+    coat: {
+        ...size(px(40)),
+        ...absolute(px(41), 0, 0, px(41)),
+        zIndex: 5,
+    },
+    decoration: {
+        display: "block",
+        ...relative(),
+        ...size(px(120)),
+        zIndex: 10,
+    },
+};
+
 const BoardCard = ({coat, isOwn = false}) => (
-    <div>
-        <img src={coat} alt={"Blason de la tribu"} />
-        <img src={isOwn ? cardBack : cardBackAlt} />
+    <div css={styles.card}>
+        <img css={styles.coat} src={coat} alt={"Blason de la tribu"} />
+        <img css={styles.decoration} src={isOwn ? cardBack : cardBackAlt} />
     </div>
 );
 
