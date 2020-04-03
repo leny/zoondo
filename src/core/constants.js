25701c905a0d3944de822bd21d3c5378e09149e0
leny <info@flatland.be>
2020-04-04 00:20:06 +0200

    Basic constants file

---
+++ src/core/constants.js
@@ -0,0 +1,20 @@
+/* leny/zoondo
+ *
+ * /src/core/constants.js - Constants
+ *
+ * coded by leny
+ * started at 03/04/2020
+ */
+
+export const ZOONDO_ENV = process.env.ZOONDO_ENV;
+export const NODE_ENV = process.env.NODE_ENV;
+export const VERSION = process.env.VERSION;
+export const BUILD_TIME = process.env.BUILD_TIME;
+
+export const NBSP = "\u00a0";
+
+export const BCG_COLOR = "black";
+export const TEXT_COLOR = "white";
+export const BORDER_COLOR = "#666";
+
+export const SOURCE_SANS_PRO_STACK = `"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif`;
