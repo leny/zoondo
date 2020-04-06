38bf470bd702dfa085d5c605efc521129a8f2ecf
leny <info@flatland.be>
2020-04-06 17:29:03 +0200

    Refactor board (add player infos & aliases)

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -6,26 +6,41 @@
  * started at 03/04/2020
  */
 
-import React from "react";
+import React, {useMemo} from "react";
 import PropTypes from "prop-types";
-import {px} from "@pwops/core";
+import {PlayerShape} from "types";
+import {px, rem} from "@pwops/core";
 import {usePwops} from "@pwops/react-hooks";
 
-import {BORDER_COLOR} from "../../core/constants";
+import {BORDER_COLOR, NBSP} from "core/constants";
 
 const ROW_SIZE = 6;
 const COL_SIZE = 6;
 const CASE_SIZE = 120;
 const GAP_SIZE = 4;
 
-const Board = ({cards = []}) => {
+const Board = ({cards = [], opponent, player}) => {
+    const boardSize = useMemo(
+        () => [
+            CASE_SIZE * COL_SIZE + (COL_SIZE - 1) * GAP_SIZE,
+            CASE_SIZE * ROW_SIZE + (ROW_SIZE - 1) * GAP_SIZE,
+        ],
+        [],
+    );
     const styles = usePwops({
+        container: {
+            flexColumn: ["flex-start", "middle"],
+            width: px(boardSize[0]),
+        },
+        player: {
+            flex: "none",
+            fontSize: rem(1.6),
+            textAlign: "center",
+        },
         board: {
-            size: [
-                px(CASE_SIZE * COL_SIZE + (COL_SIZE - 1) * GAP_SIZE),
-                px(CASE_SIZE * ROW_SIZE + (ROW_SIZE - 1) * GAP_SIZE),
-            ],
+            size: boardSize.map(px),
             flexColumnReverse: ["space-between", "middle"],
+            margin: [px(5), 0],
         },
         row: {
             flexRow: ["space-between", "middle"],
@@ -38,17 +53,36 @@ const Board = ({cards = []}) => {
     });
 
     return (
-        <div css={styles.board}>
-            {Array.from(new Array(ROW_SIZE).keys()).map(i => (
-                <div key={`row-${i}`} css={styles.row}>
-                    {Array.from(new Array(COL_SIZE).keys()).map(j => (
-                        <div key={`case-${i}-${j}`} css={styles.cell}>
-                            {(cards.find(({x, y}) => x === j && y === i) || {})
-                                .card || null}
-                        </div>
-                    ))}
-                </div>
-            ))}
+        <div css={styles.container}>
+            <div css={styles.player}>
+                <span>{"Adversaire :"}</span>
+                {NBSP}
+                <strong>{opponent.name}</strong>
+                {NBSP}
+                <span>{`(${opponent.tribe})`}</span>
+            </div>
+            <div css={styles.board}>
+                {Array.from(new Array(ROW_SIZE).keys()).map(i => (
+                    <div key={`row-${i}`} css={styles.row}>
+                        {Array.from(new Array(COL_SIZE).keys()).map(j => (
+                            <div key={`case-${i}-${j}`} css={styles.cell}>
+                                {(
+                                    cards.find(
+                                        ({x, y}) => x === j && y === i,
+                                    ) || {}
+                                ).card || null}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div css={styles.player}>
+                <span>{"Joueur :"}</span>
+                {NBSP}
+                <strong>{player.name}</strong>
+                {NBSP}
+                <span>{`(${player.tribe})`}</span>
+            </div>
         </div>
     );
 };
@@ -61,6 +95,8 @@ Board.propTypes = {
             card: PropTypes.element.isRequired,
         }),
     ),
+    player: PlayerShape,
+    opponent: PlayerShape,
 };
 
 export default Board;
