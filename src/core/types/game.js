28e6668807ec8238547e43ba09e0fd0b197faa2c
leny <info@flatland.be>
2020-04-07 21:24:07 +0200

    Define card & fighters types

--- src/core/types/game.js
+++ src/core/types/game.js
@@ -7,8 +7,30 @@
  */
 
 import PropTypes from "prop-types";
+import {FIGHTER_TYPES} from "core/constants";
 
 export const PlayerShape = PropTypes.shape({
     name: PropTypes.string.isRequired,
     tribe: PropTypes.string.isRequired,
+});
+
+export const Moves = PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.array));
+
+export const FighterVariant = PropTypes.shape({
+    corners: PropTypes.arrayOf(
+        PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    ),
+    moves: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.array)),
+});
+
+export const FighterShape = PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(FIGHTER_TYPES).isRequired,
+    variants: PropTypes.arrayOf(FighterVariant),
+    corners: PropTypes.arrayOf(
+        PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    ),
+    power: PropTypes.string,
+    value: PropTypes.number.isRequired,
+    moves: Moves,
 });
