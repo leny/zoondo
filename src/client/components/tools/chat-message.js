5465d0c799b9d4f58f729829bce796517f87126e
leny <info@flatland.be>
2020-04-10 13:20:21 +0200

    Basic chat components

---
+++ src/client/components/tools/chat-message.js
@@ -0,0 +1,88 @@
+/* leny/zoondo
+ *
+ * /src/client/components/tools/chat-message.js - ChatMessage
+ *
+ * coded by leny
+ * started at 10/04/2020
+ */
+
+/* eslint-disable react/no-danger */
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import {NBSP} from "core/constants";
+
+import marked from "marked";
+import dayjs from "dayjs";
+import {rem} from "@pwops/core";
+import {usePwops} from "@pwops/react-hooks";
+
+const getAuthorColor = (isSystem, isSelf) => {
+    if (isSystem) {
+        return "#c30";
+    }
+    return isSelf ? "cyan" : "orange";
+};
+
+const ChatMessage = ({
+    className,
+    author,
+    timestamp,
+    message,
+    isSelf,
+    isSystem,
+}) => {
+    const styles = usePwops({
+        container: {
+            flexRow: ["space-between", "flex-start"],
+            marginBottom: rem(0.5),
+            fontSize: rem(1.4),
+        },
+        infos: {
+            flex: "none",
+            marginRight: rem(1),
+        },
+        author: {
+            fontWeight: "bold",
+            color: getAuthorColor(isSystem, isSelf),
+        },
+        date: {
+            color: "silver",
+        },
+        content: {flex: [1, 0, 0]},
+        message: {
+            "em,i": {fontStyle: "italic"},
+            "strong,b": {fontWeight: "bold"},
+        },
+    });
+
+    return (
+        <div css={styles.container} className={className}>
+            <div css={styles.infos}>
+                <time css={styles.date} dateTime={dayjs(timestamp).format()}>
+                    {dayjs(timestamp).format("HH:mm")}
+                </time>
+                {NBSP}
+                <strong css={styles.author}>{isSystem ? "Jeu" : author}</strong>
+                {":"}
+            </div>
+            <div css={styles.content}>
+                <div
+                    css={styles.message}
+                    dangerouslySetInnerHTML={{__html: marked(message)}}
+                />
+            </div>
+        </div>
+    );
+};
+
+ChatMessage.propTypes = {
+    author: PropTypes.string,
+    timestamp: PropTypes.number.isRequired,
+    message: PropTypes.string.isRequired,
+    isSelf: PropTypes.bool,
+    isSystem: PropTypes.bool,
+};
+
+export default ChatMessage;
--- src/client/components/tools/chat.js
+++ src/client/components/tools/chat.js
@@ -6,17 +6,22 @@
  * started at 08/04/2020
  */
 
-import React from "react";
+import React, {useRef, useEffect} from "react";
+import PropTypes from "prop-types";
 
 import {BORDER_COLOR} from "core/constants";
-
 import {px, rem, percent, translateY} from "@pwops/core";
 import {usePwops} from "@pwops/react-hooks";
 
-const Chat = ({className}) => {
+import ChatMessage from "components/tools/chat-message";
+
+const Chat = ({className, messages = []}) => {
+    const list = useRef(null);
+
     const styles = usePwops({
         container: {
             relative: true,
+            flexColumn: ["space-between", "stretch"],
             padding: [rem(2), rem(1), rem(1)],
             border: [px(1), "solid", BORDER_COLOR],
             borderRadius: px(3),
@@ -29,14 +34,43 @@ const Chat = ({className}) => {
             fontSize: rem(1.6),
             transform: translateY(percent(-50)),
         },
+        holder: {overflow: "auto", flex: [1, 0, 0]},
+        messages: {
+            minHeight: percent(100),
+            flexColumn: ["flex-end", "stretch"],
+        },
+        message: {},
     });
+
+    useEffect(() => {
+        list.current &&
+            list.current.scrollIntoView({
+                behavior: "smooth",
+                block: "end",
+                inline: "end",
+            });
+    }, [list.current, messages]);
 
     return (
         <div css={styles.container} className={className}>
             <span css={styles.name}>{"Chat"}</span>
-            {"TODO"}
+            <div css={styles.holder}>
+                <ul css={styles.messages} ref={list}>
+                    {messages.map(message => (
+                        <li
+                            key={`${message.timestamp}-${message.author}`}
+                            css={styles.message}>
+                            <ChatMessage {...message} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
+};
+
+Chat.propTypes = {
+    messages: PropTypes.array,
 };
 
 export default Chat;
