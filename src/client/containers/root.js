d86edd386819618eefe8aa8d120c6910945a902e
leny <info@flatland.be>
2020-04-10 13:23:20 +0200

    Define player randomly

--- src/client/containers/root.js
+++ src/client/containers/root.js
@@ -11,10 +11,20 @@ import React from "react";
 import StylesGlobal from "components/styles/global";
 import Game from "containers/structure/game";
 
+const player = {
+    name: `Player (${Math.round(Math.random() * 100)})`,
+    tribe: [
+        "europa-boarix",
+        "europa-warus",
+        "europa-monkus",
+        "europa-rhinogoths",
+    ][Math.floor(Math.random() * 4)],
+};
+
 const RootContainer = () => (
     <>
         <StylesGlobal />
-        <Game />
+        <Game player={player} />
     </>
 );
 
